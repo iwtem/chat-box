0aa6eb9758552ea1b1dea7dd1acde507325e53fa
iwtem <76606053+iwtem@users.noreply.github.com>
2025-05-14 23:18:16 +0800

    feat: 新增对话框

--- app/page.tsx
+++ app/page.tsx
@@ -1,54 +1,58 @@
+import { Atom, Globe, Paperclip, Send } from 'lucide-react';
 import Image from 'next/image';
+
+import { Button } from '~/components/ui/button';
 
 export default function Home() {
   return (
-    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
-      <main className="row-start-2 flex flex-col items-center gap-[32px] sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/logo.svg"
-          alt="AI Chat Box logo"
-          width={64}
-          height={64}
-          priority
-        />
-        <ol className="list-inside list-decimal text-center font-[family-name:var(--font-geist-mono)] text-sm/6 sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{' '}
-            <code className="rounded bg-black/[.05] px-1 py-0.5 font-[family-name:var(--font-geist-mono)] font-semibold dark:bg-white/[.06]">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex flex-col items-center gap-4 sm:flex-row">
-          <a
-            className="bg-foreground text-background flex h-10 items-center justify-center gap-2 rounded-full border border-solid border-transparent px-4 text-sm font-medium transition-colors hover:bg-[#383838] sm:h-12 sm:w-auto sm:px-5 sm:text-base dark:hover:bg-[#ccc]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="flex h-10 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-4 text-sm font-medium transition-colors hover:border-transparent hover:bg-[#f2f2f2] sm:h-12 sm:w-auto sm:px-5 sm:text-base md:w-[158px] dark:border-white/[.145] dark:hover:bg-[#1a1a1a]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <main className="flex h-full flex-col">
+      <div className="relative m-auto flex w-full max-w-200 flex-col gap-6">
+        <div className="mx-auto flex items-center gap-4">
+          <Image
+            className="dark:invert"
+            src="/logo.svg"
+            alt="AI Chat Box logo"
+            width={64}
+            height={64}
+            priority
+          />
+          <h1 className="text-2xl font-medium">Hi, I&apos;m AI Chat Box.</h1>
         </div>
-      </main>
-    </div>
+
+        <p className="mx-auto text-center text-gray-500">How can I help you today?</p>
+
+        <div className="flex w-full flex-col gap-2 rounded-3xl border border-gray-100 bg-gray-50 p-4">
+          <textarea
+            rows={2}
+            placeholder="Ask me anything..."
+            className="resize-none placeholder:text-gray-300 focus:outline-none"
+          />
+          <div className="flex justify-between gap-4">
+            <div className="flex gap-2">
+              <Button variant="outline" className="rounded-full">
+                <Atom />
+                深度思考
+              </Button>
+              <Button variant="outline" className="rounded-full">
+                <Globe />
+                联网搜索
+              </Button>
+            </div>
+            <div>
+              <Button variant="ghost" size="icon" className="rounded-full">
+                <Paperclip />
+              </Button>
+              <Button variant="outline" size="icon" className="rounded-full">
+                <Send />
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="fixed bottom-0 w-full p-2 text-center text-xs text-gray-400">
+        AI-generated, for reference only
+      </div>
+    </main>
   );
 }
