c52a8ec256ccf129ceb528c332162ced40768acb
huibin <iwtem@aliyun.com>
2025-05-14 11:22:56 +0800

    chore: 调整 vscode 配置，调整 eslint 配置

---
+++ eslint.config.ts
@@ -0,0 +1,16 @@
+import { FlatCompat } from '@eslint/eslintrc';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({ baseDirectory: __dirname });
+
+const eslintConfig = defineConfig([
+  ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  globalIgnores(['.next', 'node_modules', 'eslint.config.ts']),
+]);
+
+export default eslintConfig;
