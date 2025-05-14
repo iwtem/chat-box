451364f42212ddacb6a7bf4a7c2c3182f677d462
huibin <iwtem@aliyun.com>
2025-05-14 17:33:38 +0800

    feat: add eslint-plugin-prettier，对于 prettier 格式错误，增加 eslint 检查

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,6 +2,7 @@ import { FlatCompat } from '@eslint/eslintrc';
 import { defineConfig, globalIgnores } from 'eslint/config';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,6 +11,7 @@ const compat = new FlatCompat({ baseDirectory: __dirname });
 
 const eslintConfig = defineConfig([
   ...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
+  eslintPluginPrettierRecommended,
   globalIgnores(['.next', 'node_modules', 'eslint.config.ts']),
 ]);
 
