008dd4ff2c064abceb2d197d77b4caaa42bd3196
huibin <iwtem@aliyun.com>
2025-05-21 18:06:37 +0800

    feat: add mock data

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,45 @@
 'use client';
 
-import { useChat } from '@ai-sdk/react';
+import { Message, useChat } from '@ai-sdk/react';
+import { useSearchParams } from 'next/navigation';
 
 import Hero from '~/components/chat/hero';
 import MessageInput from '~/components/chat/message-input';
 import Messages from '~/components/chat/messages';
-import { cn } from '~/lib/utils';
+import mockMessages from '~/mock/messages.json';
 
 export default function Home() {
-  const { messages, input, handleInputChange, handleSubmit } = useChat();
+  const searchParams = useSearchParams();
+  const { messages, input, handleInputChange, handleSubmit } = useChat({
+    initialMessages: searchParams.get('mock') ? (mockMessages as unknown as Message[]) : [],
+  });
   const hasMessages = messages.length > 0;
 
   return (
-    <main className="flex h-full">
-      <div className="mx-auto flex w-full max-w-200 flex-col justify-center gap-6 px-4 md:px-12">
-        {hasMessages ? <Messages messages={messages} className="flex-1" /> : <Hero />}
-
-        <div className="sticky bottom-0 w-full flex-none bg-white">
+    <main className="mx-auto h-full w-full max-w-200 px-4 md:px-12">
+      {hasMessages ? (
+        <div className="flex h-full flex-col">
+          <Messages messages={messages} className="flex-1" />
+          <div className="sticky bottom-0 w-full flex-none bg-white">
+            <MessageInput
+              input={input}
+              handleInputChange={handleInputChange}
+              handleSubmit={handleSubmit}
+              className="shadow-md"
+            />
+            <div className="py-2 text-center text-xs text-gray-400">内容由 AI 生成，请仔细甄别</div>
+          </div>
+        </div>
+      ) : (
+        <div className="flex h-full flex-col justify-center gap-6">
+          <Hero />
           <MessageInput
             input={input}
             handleInputChange={handleInputChange}
             handleSubmit={handleSubmit}
-            className={cn(hasMessages ? 'shadow-md' : '')}
           />
-          {hasMessages && (
-            <div className="py-2 text-center text-xs text-gray-400">
-              AI-generated, for reference only
-            </div>
-          )}
         </div>
-      </div>
+      )}
     </main>
   );
 }
